598707295b1ffeaaa5108b9a388da70767f9666e
JRPino <pino3789@gmail.com>
2020-06-22 17:50:43 -0700

    Added Wrapper.js for children

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Header from "./components/Header";
 
 // import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Wrapper>
+        <Header />
+      </Wrapper>
     </div>
   );
 }
