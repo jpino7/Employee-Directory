05333756173019fec260fd7971558e6f30c4eb02
JRPino <pino3789@gmail.com>
2020-06-22 22:01:50 -0700

    Updates

---
+++ precache-manifest.341af989618c8eaf6e7239226572f24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2803980014009c0060baff056c5da2",
+    "url": "/jpino7/Employee-Directory/index.html"
+  },
+  {
+    "revision": "b6d7e78a13d11241f52d",
+    "url": "/jpino7/Employee-Directory/static/css/main.f1477fdc.chunk.css"
+  },
+  {
+    "revision": "515a5975f9579ae583bc",
+    "url": "/jpino7/Employee-Directory/static/js/2.4faaf149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jpino7/Employee-Directory/static/js/2.4faaf149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d7e78a13d11241f52d",
+    "url": "/jpino7/Employee-Directory/static/js/main.56b81ab2.chunk.js"
+  },
+  {
+    "revision": "d1fdb5766c3c7c8ab20d",
+    "url": "/jpino7/Employee-Directory/static/js/runtime-main.a2396d81.js"
+  }
+]);
